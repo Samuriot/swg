6b1f18aabd8b1b70774f1deb53392b98dc38df79
Samuriot <92127962+Samuriot@users.noreply.github.com>
2026-04-02 20:15:32 -0700

    added exec_command

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 #include <sstream>
 #include <vector>
 
+#include <unistd.h>
+#include <sys/wait.h>
+
 void query_command();
 std::vector<std::string> parse_command(const std::string& str);
-int exec_command();
+int exec_command(const std::vector<std::string>& v);
 
-int main(int argc, char** argv) {
+int main() {
   query_command();
   return 0;
 }
@@ -20,11 +23,17 @@ void query_command() {
   while(true) {
     std::string prompt;
     std::cout << "$ " << std::flush;
+    // handles getline failure (i.e.: ctrl-d out of program)
     if(!std::getline(std::cin, prompt))
       break;
+    // continue if you do not type anything
     if(prompt == "") 
       continue;
     std::vector<std::string> tokens = parse_command(prompt);
+    if(tokens.empty())
+      continue;
+    
+    exec_command(tokens);
   }
 }
 
@@ -34,12 +43,43 @@ std::vector<std::string> parse_command(const std::string& str) {
 
   std::vector<std::string> tokens;
   while(parser >> token) {
-    tokens.push_back(token);
+    tokens.emplace_back(token);
   } 
 
   return tokens;
 }
 
-int exec_command() {
+int exec_command(const std::vector<std::string>& v) {
+  if(v.empty()) 
+    return 0;
+  // setup child process
+  pid_t pid = fork();
+
+  // case child
+  if(pid == 0) {
+    // setup a new char* vector for c strings
+    std::vector<char*> args;
+    args.reserve(v.size() + 1);
+
+    // push content of v into args & convert to c_str
+    for(const auto& token : v) 
+      args.emplace_back(const_cast<char*>(token.c_str()));
+   
+    // null-terminate data
+    args.emplace_back(nullptr);
+
+    // execute & exit
+    execvp(args[0], args.data());
+    perror("execvp");
+    _exit(1);
+
+  } else if(pid > 0) { // case parent
+    // wait for child 
+    int status = 0;
+    if(waitpid(pid, &status, 0) == -1)
+      perror("waitpid");
+  } else // case fork error
+    perror("fork");
+  
   return 0;
 }
