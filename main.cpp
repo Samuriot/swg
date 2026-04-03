6a32a7f278e74fbbc925d862d6e5a92240c4349f
Samuriot <92127962+Samuriot@users.noreply.github.com>
2026-04-02 19:37:40 -0700

    added a parsing command to parse a command into tokens

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 void query_command();
+std::vector<std::string> parse_command(std::string& str);
 
 int main(int argc, char** argv) {
   query_command();
@@ -15,8 +18,28 @@ int main(int argc, char** argv) {
 void query_command() {
   while(1) {
     std::string prompt;
+    std::vector<std::string> tokens;
     std::cout << "$ " << std::flush;
-    std::cin >> prompt;
-    std::cout << prompt << std::endl;
+    std::getline(std::cin, prompt);
+    if(prompt == "") {
+      continue;
+    }
+    tokens = parse_command(prompt);
+    for(auto& token: tokens) {
+      std::cout << token << " ";
+    }
+    std::cout << std::endl;
   }
+}
+
+std::vector<std::string> parse_command(std::string& str) {
+  std::istringstream parser(str);
+  std::string token;
+
+  std::vector<std::string> tokens;
+  while(parser >> token) {
+    tokens.push_back(token);
+  } 
+
+  return tokens;
 }
